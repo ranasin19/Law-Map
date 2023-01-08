2db70630fde2879a512b61431b509285faddb556
une <ranaapps.41@gmail.com>
2023-01-08 10:45:25 +0300

    Initial Commit

---
+++ lawapp/lawappApp.swift
@@ -0,0 +1,17 @@
+//
+//  lawappApp.swift
+//  lawapp
+//
+//  Created by rana omar zaheri on 15/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct lawappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
